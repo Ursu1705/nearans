11359170a132ecb1b2e5014524bd5c4859d5c129
Paty Arakawa <paty.arakawa@gmail.com>
2017-06-24 04:50:29 -0500

    mlab

--- app.js
+++ app.js
@@ -13,9 +13,21 @@ require('./models/user');
 require('./passport')(passport);
 
 // Conexión a la base de datos de MongoDB que tenemos en local
-mongoose.connect('mongodb://localhost:27017/passport-example', function(err, res) {
+/*mongoose.connect('mongodb://localhost:27017/passport-example', function(err, res) {
   if(err) throw err;
   console.log('Conectado con éxito a la BD');
+});*/
+
+var uristring = process.env.MONGOLAB_URI ||
+  'mongodb://localhost:27017/passport-example';
+
+// **************  Conexión a la base de datos
+mongoose.connect(uristring, function (err, res) {
+  if (err) { 
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
 });
 
 // Iniciamos la aplicación Express
